ac41c3195f418c7ad29824197c2d4286242195ba
Alexandr Pavlov <cvetitluna@yandex.ru>
2020-05-06 18:20:25 +0400

    Добавлены ДЗ 2 урока 3 секции

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,7 @@
 
 T
 $Section1/TestIT_AddStudentsLogins.py,9\a\9a3bb4b6e2c69b533cd93a1fbe231145534368a1
+V
+&Section1/1lesson6_step11_codeReview.py,5\b\5b33ab192c921d1a555e7f522d230d6a7fbd169b
+S
+#Section3/3lesson2_step13_unitest.py,4\b\4be5ed91a9b15db478a74484e869833f2121cdeb
---
+++ Section3/3lesson2_step13_unitest.py
@@ -0,0 +1,72 @@
+# encoding utf8
+from selenium import webdriver
+import time
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def test_fist(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        name = browser.find_element_by_css_selector("input.form-control.first[required]")
+        name.send_keys("Name")
+        secondName = browser.find_element_by_css_selector("input.form-control.second[required]")
+        secondName.send_keys("Second Name")
+        email = browser.find_element_by_css_selector("input.form-control.third[required]")
+        email.send_keys("email@mail.com")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+        time.sleep(10)
+        browser.quit()
+
+    def test_second(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        name = browser.find_element_by_css_selector("input.form-control.first[required]")
+        name.send_keys("Name")
+        secondName = browser.find_element_by_css_selector("input.form-control.second[required]")
+        secondName.send_keys("Second Name")
+        email = browser.find_element_by_css_selector("input.form-control.third[required]")
+        email.send_keys("email@mail.com")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+        time.sleep(10)
+        browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
