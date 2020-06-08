82d1dcf13b4dcbabf8dc7fbbd9ad10cd0e16d54e
PavlovAA <aapavlov988@gmail.com>
2020-06-09 00:24:10 +0400

    [+]Добавлено ожидание и текст ошибки

--- test_items.py
+++ test_items.py
@@ -5,5 +5,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_verify_add_to_basket_button_present(browser):
     browser.get(link)
-    time.sleep(30)
-    assert len(browser.find_elements_by_css_selector("form#add_to_basket_form button[type=submit]")) > 0
+    #time.sleep(30)
+    assert len(browser.find_elements_by_css_selector("form#add_to_basket_form button[type=submit]")) > 0, \
+        "На странице отсутствует кнопка добавления товара в корзину"
